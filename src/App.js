793165cac6c1db9614f0fd638be2f7a805625b16
Abhishek Kandi <kandii.abhishek@gmail.com>
2025-04-29 09:51:37 +0530

    Added Footer & Header components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import './styles.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MovieGrid from './components/MovieGrid'
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Moviedux</h1>
-      </header>
-      <footer className="footer">
-        <p className="footer">Footer content here</p>
-      </footer>
+      <div className='container'>
+        <Header></Header>
+        <MovieGrid></MovieGrid>
+      </div>
+      
+      <Footer></Footer>
     </div>
   );
 }
